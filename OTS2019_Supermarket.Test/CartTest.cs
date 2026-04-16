9e64417c3fae71c63aeff504916cce74d37aa028
KataBanci <katabanci4@gmail.com>
2026-04-16 19:49:25 +0200

    vezbe 5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,29 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCard_ShouldAddItemToCart_Success() //naziv klasse koju testiram sta testiram i sta ocekujem
+        {
 
+            //ARRANGE pred uslovi
+
+            Cart cart = new Cart();
+            //ako dodam da je cart.size=5 onda dole ne moze biti 1 nego ce biti 6 
+            Monitor monitor = new Monitor();
+
+            //ACT poziv metode koje zelimo da testiramo
+
+            cart.AddOneToCart(monitor);
+
+            //ASSERT nesto gde treba da proverimo, da testiramo
+
+            Assert.That(cart.Size, Is.EqualTo(1)); //druga vredsnost je sta ocekujem da bude
+            Assert.That(cart.Amount, Is.EqualTo(100));
+
+
+        }
     }
 }
