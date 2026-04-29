e7b64419e8f6be26593230c6cb4d206d8a95c807
KataBanci <katabanci4@gmail.com>
2026-04-29 14:59:08 +0200

    vezba 6 testovi

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -1,10 +1,6 @@
 ﻿using NUnit.Framework;
-using OTS_Supermarket.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Monitor = OTS_Supermarket.Models.Monitor;
 
 namespace OTS_Supermarket.Test
 {
@@ -12,25 +8,175 @@ namespace OTS_Supermarket.Test
     public class CartTest
     {
         [Test]
-        public void AddOneToCard_ShouldAddItemToCart_Success() //naziv klasse koju testiram sta testiram i sta ocekujem
+        public void AddOneToCart_OneMonitorToCart_Success()
         {
-
-            //ARRANGE pred uslovi
-
             Cart cart = new Cart();
-            //ako dodam da je cart.size=5 onda dole ne moze biti 1 nego ce biti 6 
             Monitor monitor = new Monitor();
-
-            //ACT poziv metode koje zelimo da testiramo
 
             cart.AddOneToCart(monitor);
 
-            //ASSERT nesto gde treba da proverimo, da testiramo
+            Assert.That(1, Is.EqualTo(cart.Monitor_counter));
+        }
 
-            Assert.That(cart.Size, Is.EqualTo(1)); //druga vredsnost je sta ocekujem da bude
-            Assert.That(cart.Amount, Is.EqualTo(100));
+        [Test]
+        public void AddOneToCart_OneMonitorToCart_Size()
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
 
+            cart.AddOneToCart(monitor);
 
+            Assert.That(1, Is.EqualTo(cart.Size));
+        }
+
+        [Test]
+        public void AddOneToCart_OneMonitorToCartSizeAlready10_Exception()
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            cart.Size = 10;
+
+            Exception exception = Assert.Throws<Exception>(() => cart.AddOneToCart(monitor));
+
+            Assert.That(exception.Message, Is.EqualTo("Number of items in cart must be 10 or less!"));
+        }
+
+        [TestCase(5, 6)]
+        [TestCase(6, 7)]
+        public void AddOneToCart_OneMonitorToCart_SuccessDataDriven(int counter, int expectedResult)
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            cart.Size = counter;
+
+            cart.AddOneToCart(monitor);
+
+            Assert.That(expectedResult, Is.EqualTo(cart.Size));
+        }
+
+        [TestCase(5, ExpectedResult = 6)]
+        [TestCase(6, ExpectedResult = 7)]
+        public int AddOneToCart_OneMonitorToCart_SuccessDataDrivenWithReturnValue(int counter)
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            cart.Size = counter;
+
+            cart.AddOneToCart(monitor);
+
+            return cart.Size;
+        }
+
+        [TestCaseSource(typeof(CartTxtParser), "GetTestCasesData", new object[] { "PICTResults.txt" })]
+        public void Test(int size, int amount, int laptop, int monitor, int chair, int computer, int keyboard, string stringDate, double discount)
+        {
+            Cart cart = new Cart();
+            cart.Size = size;
+            cart.Monitor_counter = monitor;
+            cart.Laptop_counter = laptop;
+            cart.Computer_counter = computer;
+            cart.Keyboard_counter = keyboard;
+            cart.Chair_counter = chair;
+            cart.Amount = amount;
+            cart.Budget = 100000;
+
+            cart.Calculate(stringDate);
+
+            double expectedFinalBudget = 100000 - (amount - amount * discount);
+            Assert.That(cart.Budget, Is.EqualTo(expectedFinalBudget).Within(0.001));
+        }
+
+        [Test]
+        public void NewCart_SizeIsZero()
+        {
+            Cart cart = new Cart();
+
+            Assert.That(cart.Size, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NewCart_MonitorCounterIsZero()
+        {
+            Cart cart = new Cart();
+
+            Assert.That(cart.Monitor_counter, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AddOneToCart_TwoMonitors_MonitorCounterIsTwo()
+        {
+            Cart cart = new Cart();
+            Monitor monitor1 = new Monitor();
+            Monitor monitor2 = new Monitor();
+
+            cart.AddOneToCart(monitor1);
+            cart.AddOneToCart(monitor2);
+
+            Assert.That(cart.Monitor_counter, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AddOneToCart_TwoMonitors_SizeIsTwo()
+        {
+            Cart cart = new Cart();
+            Monitor monitor1 = new Monitor();
+            Monitor monitor2 = new Monitor();
+
+            cart.AddOneToCart(monitor1);
+            cart.AddOneToCart(monitor2);
+
+            Assert.That(cart.Size, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AddOneToCart_SizeNine_SuccessSizeBecomesTen()
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            cart.Size = 9;
+
+            cart.AddOneToCart(monitor);
+
+            Assert.That(cart.Size, Is.EqualTo(10));
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        [TestCase(8, 9)]
+        [TestCase(9, 10)]
+        public void AddOneToCart_SizeIncreasesByOne(int size, int expectedSize)
+        {
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            cart.Size = size;
+
+            cart.AddOneToCart(monitor);
+
+            Assert.That(cart.Size, Is.EqualTo(expectedSize));
+        }
+
+        [Test]
+        public void Calculate_AmountZero_BudgetStaysSame()
+        {
+            Cart cart = new Cart();
+            cart.Amount = 0;
+            cart.Budget = 100000;
+
+            cart.Calculate("2024-01-01");
+
+            Assert.That(cart.Budget, Is.EqualTo(100000).Within(0.001));
+        }
+
+        [Test]
+        public void Calculate_AmountWithoutDiscount_BudgetIsReduced()
+        {
+            Cart cart = new Cart();
+            cart.Amount = 5000;
+            cart.Budget = 100000;
+
+            cart.Calculate("2024-01-01");
+
+            Assert.That(cart.Budget, Is.LessThan(100000));
         }
     }
 }
